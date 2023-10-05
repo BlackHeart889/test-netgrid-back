9b80c70c7a36297349bb1b13305201787f2e2a22
Cristhian Monrroy <monrroy88960@hotmail.com>
2023-10-05 14:08:58 -0500

    Configuracion roles y permisos

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+use App\Models\Proyectos\{
+    Proyecto,
+    UsuarioProyecto,
+};
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +26,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'nombre',
         'email',
         'password',
     ];
@@ -30,7 +38,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        // 'remember_token',
     ];
 
     /**
@@ -39,7 +47,17 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        // 'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function proyectosCreados(): HasMany
+    {
+        return $this->hasMany(Proyecto::class, 'id_usuario', 'id');
+    }
+
+    public function proyectosAsignados(): BelongsToMany
+    {
+        return $this->belongsToMany(Proyecto::class, 'usuariosproyectos', 'id_usuario', 'id_proyecto')->using(UsuarioProyecto::class);
+    }
 }
