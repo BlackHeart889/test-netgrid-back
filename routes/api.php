fd3fc21a7799388884ffc38c1580ebafa96ae794
Cristhian Monrroy <monrroy88960@hotmail.com>
2023-10-05 14:09:34 -0500

    Rutas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\{
+    LoginController,
+    ProyectosController,
+};
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +22,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Recordar que:
+ *
+ * Para solicitudes post, se debe generar primero el Token CSRF a través de
+ * /sanctum/csrf-token
+ */
+Route::middleware(['web'])->group(function () {
+    Route::post('/register', [LoginController::class, 'register']);
+    Route::post('/login', [LoginController::class, 'authenticate']);
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('/logout', [LoginController::class, 'logout']);
+
+        Route::post('/proyectos/crear', [ProyectosController::class, 'crearProyecto']);
+        Route::delete('/proyectos/eliminar', [ProyectosController::class, 'eliminarProyecto']);
+
+        Route::get('/proyectos/proyectos-creados', [ProyectosController::class, 'proyectosCreados']);
+        Route::get('/proyectos/proyectos-asignados', [ProyectosController::class, 'proyectosAsignados']);
+
+
+        Route::patch('/proyectos/actualizar', [ProyectosController::class, 'actualizarProyecto']);
+        Route::post('/proyectos/asignar-proyecto', [ProyectosController::class, 'asignarProyecto']);
+    });
+});
+
